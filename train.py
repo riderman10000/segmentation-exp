a9e164f107b2c47827616edc1cea9edc74e3c7bc
unknown <rimeshlwagun10@gmail.com>
2024-05-03 12:17:56 +0545

    adding custom albumentation

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import os 
+import cv2
 import numpy as np 
 from tqdm.auto import tqdm
 import matplotlib.pyplot as plt
@@ -104,11 +105,24 @@ def valid(epoch, model: SegmentationModel, batch_size, image_size, data_loader, 
             # plt.show()
         return infer_loss / len(data_loader), infer_accuracy/len(data_loader)
 
+def letterbox(image, **kwargs):
+    resized_width, resized_height = kwargs['resized_width'][0], kwargs['resized_height'][0]
+    height, width, _ = image.shape
+    scale = min(resized_height / height, resized_width / width)
+    new_height = int(height * scale)
+    new_width = int(width * scale)
+    image = cv2.resize(image, (new_width, new_height))
+    new_img = np.full((resized_height, resized_width, 3), 128, dtype='uint8')
+    # fill new image with the resized image and centered it
+    new_img[(resized_height - new_height) // 2:(resized_height - new_height) // 2 + new_height,
+            (resized_width - new_width) // 2:(resized_width - new_width) // 2 + new_width,
+            :] = image.copy()
+    return new_img 
 
 if __name__ == "__main__":
     image_size = 512 
     epochs = 100
-    batch_size = 6
+    batch_size = 4
     learning_rate = 0.0003
     
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -116,12 +130,21 @@ if __name__ == "__main__":
     
     n_times = 1 # increase the datasize by n times
 
-    save_dirs = './runs/exp-no-augments/iteration-3/'
-
+    save_dirs = './runs/exp-augments-geometry/iteration-1/'
+    # albumentation_letterbox = 
+    def custom_transform(data, **kwargs):
+        kwargs['resized_width'] = image_size,
+        kwargs['resized_height'] = image_size,
+        return letterbox(image=data, **kwargs)
+    
     transformations = A.Compose([
-        A.Resize(image_size, image_size),
-        # A.HorizontalFlip(p=0.5),
-        # A.VerticalFlip(p=0.5),
+        # A.Resize(image_size, image_size),
+        A.Lambda(name='Letter Box', 
+            image=custom_transform,
+            mask=custom_transform,
+            p=1),
+        A.HorizontalFlip(p=0.5),
+        A.VerticalFlip(p=0.5),
         # A.Blur(blur_limit=(1, 5), p=0.6),
         # A.CLAHE(clip_limit=(1, 4), tile_grid_size=(8, 8), p=0.25),
         # A.RandomBrightnessContrast(p=0.5),
