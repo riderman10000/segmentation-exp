954a0db47173dc358c6ae682048a8b519a6329e8
unknown <rimeshlwagun10@gmail.com>
2024-05-05 16:53:01 +0545

    added class number as argument and removed loss calculation to metrics

--- model.py
+++ model.py
@@ -3,21 +3,21 @@ import torch
 from torch import nn 
 
 class SegmentationModel(nn.Module):
-    def __init__(self, encoder = 'timm-efficientnet-b0', weights = 'imagenet') -> None:
+    def __init__(self, encoder = 'timm-efficientnet-b0', weights = 'imagenet', in_channels = 3, classes = 10) -> None:
         super(SegmentationModel, self).__init__()
         self.model = smp.Unet(
             encoder_name=encoder, 
             encoder_weights=weights, 
-            in_channels=3, 
-            classes=3
+            in_channels= in_channels, 
+            classes= classes
         )
         # self.loss_fn = smp.losses.DiceLoss(mode='multiclass', from_logits=True)
         
     def forward(self, image, mask = None):
         pred = self.model(image) 
-        if mask != None:  
-            # print('argmax', torch.argmax(mask, dim=1))
-            # print('shape', torch.argmax(mask, dim=1).shape, mask.shape)
-            loss =  nn.CrossEntropyLoss()(pred, torch.argmax(mask, dim=1)) 
-            return pred, loss 
+        # if mask != None:  
+        #     # print('argmax', torch.argmax(mask, dim=1))
+        #     # print('shape', torch.argmax(mask, dim=1).shape, mask.shape)
+        #     loss =  nn.CrossEntropyLoss()(pred, torch.argmax(mask, dim=1)) 
+        #     return pred, loss 
         return pred 
